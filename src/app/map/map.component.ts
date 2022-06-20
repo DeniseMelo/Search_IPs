48e6e0fe3c60b00faf26e4d66f08db1df442f958
DeniseMelo <melodenise.ti@gmail.com>
2022-06-19 21:15:08 -0300

    API ipify traz os IPs na Search

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,30 +1,29 @@
 import { MapService } from './../map.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
   map: L.Map | undefined;
 
   constructor (public MapService : MapService) { }
 
 
-  ngOnInit(): void {
-    this.MapService.getCoordenadas();
-  }
-  /*ngAfterViewInit(): void {
+
+  ngAfterViewInit(): void {
     this.initMap();
+
   }
   private initMap(): void {
     this.map = L.map('map').setView([-23.2927,  -51.1732],15)
    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
-}).addTo(this.map);
-  }*/
+});tiles.addTo(this.map);
+  }
 
 
 }
